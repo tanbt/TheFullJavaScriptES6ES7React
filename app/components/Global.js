34546af83c043f373931d05eb9b03425db8d27b6
Tan Bui <trungtanbui@gmail.com>
2017-11-18 17:35:00 +0200

    Add property for component with get/set

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
     search() {
-        console.log('search');
+        console.log(this.state.query);
     }
 
     render() {
@@ -12,7 +19,7 @@ class Global extends Component {
                 <h2>Book Explorer!</h2>
                 <FormGroup>
                     <InputGroup>
-                        <FormControl type="text" placeholder="Search for a book"/>
+                        <FormControl type="text" placeholder="Search for a book" onChange={event => this.setState({query: event.target.value})}/>
                         <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
