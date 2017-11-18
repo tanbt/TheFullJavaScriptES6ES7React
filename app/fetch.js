462d9c99617a653ad2f4cfd6f73fdd7b9a791ae4
Tan Bui <trungtanbui@gmail.com>
2017-11-18 16:51:54 +0200

    REACT app - install dependencies

--- app/fetch.js
+++
@@ -1,7 +0,0 @@
-const root = "https://jsonplaceholder.typicode.com/posts/1";
-
-fetch(root, {
-        method: "GET"
-    }) //return a promise
-    .then(response => response.json()) //return a promise
-    .then(json => console.log(json));
