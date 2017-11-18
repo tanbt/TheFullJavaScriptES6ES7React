741c053d4b80f12c67f396b1ccb62a2c2ae7da4f
Tan Bui <trungtanbui@gmail.com>
2017-11-18 17:21:07 +0200

    The first global component

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Global from './components/Global';
 
-ReactDOM.render( <div> React Application </div>,
+ReactDOM.render( 
+    <Global/>,
     document.getElementById('root')
 );
